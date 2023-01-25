6152aa2974eefa28607edd3301007eb31beb96b6
rezapfebriyan <rezafebriyan00@gmail.com>
2023-01-25 12:12:44 +0700

    set file controllers

---
+++ controllers/ProductController.js
@@ -0,0 +1,21 @@
+import Product from "../models/Product.js"
+
+export const getProducts = (req, res) => {
+    //
+}
+
+export const showProduct = (req, res) => {
+    //
+}
+
+export const createProduct = (req, res) => {
+    //
+}
+
+export const updateProduct = (req, res) => {
+    //
+}
+
+export const deleteProduct = (req, res) => {
+    //
+}
---
+++ controllers/UserController.js
@@ -0,0 +1,21 @@
+import User from "../models/User"
+
+export const getUsers = (req, res) => {
+    //
+}
+
+export const showUser = (req, res) => {
+    //
+}
+
+export const createUser = (req, res) => {
+    //
+}
+
+export const updateUser = (req, res) => {
+    //
+}
+
+export const deleteUser = (req, res) => {
+    //
+}
