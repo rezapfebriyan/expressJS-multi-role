f6acababf8b3948bf31ee71e7bed05480570b416
rezapfebriyan <rezafebriyan00@gmail.com>
2023-01-31 10:21:08 +0700

    add endpoint auth login and logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,10 +1,11 @@
 import User from "../models/User.js"
 import argon2 from "argon2"
+import { where } from "sequelize"
 
 export const Login = async (req, res) => {
     const user = await User.findOne({
         where: {
-            email: req.params.email
+            email: req.body.email
         }
     })
 
@@ -29,14 +30,46 @@ export const Login = async (req, res) => {
     const email = user.email
     const role = user.role
     res.status(200).json({
-            "Data" : { uuid, name, email, role }
-        })
+        "Status code" : 200,
+        message : "Login successful",
+        "Data" : { uuid, name, email, role }
+    })
+}
+
+export const userAuth = async (req, res) => {
+    if (!req.session.userId) {
+        return res.status(401).json({ message : "You must login" })
+    }
+
+    const user = await User.findOne({
+        attributes: [ 'uuid', 'name', 'email', 'role' ],
+        where: {
+            uuid: req.session.userId
+        }
+    })
+
+    if (!user) return res.status(404).json({
+        "Status code" : 404,
+        message: "User not found"
+    })
+
+    res.status(200).json({
+        "Status code" : 200,
+        "Data" : user
+    })
 }
 
 export const Logout = (req, res) => {
     //* delete session
     req.session.destroy((error) => {
-        if (error) return res.status(400).json({ message: "Logout failed" })
-        res.status(200).json({ message: "Lougout success" })
+        if (error) return res.status(400).json({ 
+            "Status code" : 400,
+            message: "Logout failed"
+        })
+
+        res.status(200).json({ 
+            "Status code" : 200,
+            message: "Logout successful"
+        })
     })
 }
