088a3589615d687eedc75bde51c4fdbb6a374d1f
rezapfebriyan <rezafebriyan00@gmail.com>
2023-02-26 04:19:38 +0700

    add endpoint create and show product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js"
 import User from "../models/User.js"
+import {Op} from "sequelize"
 
 export const getProducts = async (req, res) => {
     try {
@@ -8,14 +9,22 @@ export const getProducts = async (req, res) => {
         //* if auth user admin
         if (req.role === "Admin") {
             response = await Product.findAll({
-                include:[{ model: User }] //? ada relasi Product ke User
+                attributes:['uuid', 'name', 'price'],
+                include:[{ 
+                    model: User,
+                    attributes:['uuid', 'name']
+                 }] //? ada relasi Product ke User
             })
         } else {
             response = await Product.findAll({
+                attributes:['uuid', 'name', 'price'],
                 where: {
                     userId: req.userId //? req from data middleware
                 },
-                include:[{ model: User }] //? ada relasi Product ke User
+                include:[{ 
+                    model: User,
+                    attributes:['uuid', 'name']
+                }] //? ada relasi Product ke User
             })
         }
 
@@ -30,12 +39,68 @@ export const getProducts = async (req, res) => {
     }
 }
 
-export const showProduct = (req, res) => {
-    //
+export const showProduct = async (req, res) => {
+    try {
+        const product = await Product.findOne({
+            where: {uuid: req.params.id}
+        })
+        if (!product) return res.status(404).json({
+            "Status code" : 404,
+            message: "Product not found" 
+        })
+
+        let response
+
+        //* if auth user admin
+        if (req.role === "Admin") {
+            response = await Product.findOne({
+                attributes:['uuid', 'name', 'price'],
+                where: {id: product.id},
+                include:[{ 
+                    model: User,
+                    attributes:['uuid', 'name']
+                 }] //? ada relasi Product ke User
+            })
+        } else {
+            response = await Product.findOne({
+                attributes:['uuid', 'name', 'price'],
+                where: {
+                    [Op.and]: [
+                        {id: product.id}, {userId: req.userId}
+                    ]
+                },
+                include:[{ 
+                    model: User,
+                    attributes:['uuid', 'name']
+                }]
+            })
+        }
+
+        res.status(200)
+            .json({ "Status code" : 200,
+                "Data": response })
+
+    } catch (error) {
+        res.status(500)
+            .json({ "Status code" : 500,
+            message: error.message })
+    }
 }
 
-export const createProduct = (req, res) => {
-    //
+export const createProduct = async (req, res) => {
+    const {name, price} = req.body
+    try {
+        await Product.create({
+            name,
+            price,
+            userId: req.userId
+        })
+        res.status(201)
+        .json({ "Status code" : 201,
+            message: "Product has been created" })
+    } catch (error) {
+        
+    }
 }
 
 export const updateProduct = (req, res) => {
