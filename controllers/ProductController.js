ff237010fda6d213881d93ee717fa3a9700e9fc1
rezapfebriyan <rezafebriyan00@gmail.com>
2023-02-25 13:33:44 +0700

    add endpoint get products

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,7 +1,33 @@
 import Product from "../models/Product.js"
+import User from "../models/User.js"
 
-export const getProducts = (req, res) => {
-    //
+export const getProducts = async (req, res) => {
+    try {
+        let response
+
+        //* if auth user admin
+        if (req.role === "Admin") {
+            response = await Product.findAll({
+                include:[{ model: User }] //? ada relasi Product ke User
+            })
+        } else {
+            response = await Product.findAll({
+                where: {
+                    userId: req.userId //? req from data middleware
+                },
+                include:[{ model: User }] //? ada relasi Product ke User
+            })
+        }
+
+        res.status(200)
+            .json({ "Status code" : 200,
+                "Data": response })
+
+    } catch (error) {
+        res.status(500)
+            .json({ "Status code" : 500,
+            message: error.message })
+    }
 }
 
 export const showProduct = (req, res) => {
